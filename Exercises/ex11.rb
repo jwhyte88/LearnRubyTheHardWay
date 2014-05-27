0d69ddbd9260ccd556a37ecc05443fe50c3114cd
jwhyte88 <jwhyte@opentable.com>
2014-05-27 13:45:15 +0100

    added ex11.rb and ex12.rb

---
+++ Exercises/ex11.rb
@@ -0,0 +1,33 @@
+# Exercise 11: Asking Questions
+
+# The default exercise code is below
+
+print "How old are you? "
+age = gets.chomp()
+print "How tall are you? "
+height = gets.chomp()
+print "How much do you weigh? "
+weight = gets.chomp()
+
+puts "So, you're #{age} years old, #{height} tall and #{weight} heavy."
+
+# Extra Credit exercises are below
+
+# print "this is a test?"
+# test1 = gets
+# puts "so test? #{test1}"
+
+# print "this is also a test?"
+# test2 = gets.chomp
+# puts "so this is also a test? #{test2}"
+
+# print "what is your favourite football club?"
+# club = gets.chomp()
+# print "Who is your favourite player?"
+# player = gets.chomp()
+# print "Why?"
+# reason = gets.chomp()
+
+# puts "so, your favourite football club is #{club}, and player is #{player}. Why? because #{reason}"
+
+# Notes
